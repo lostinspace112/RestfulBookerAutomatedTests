e5f310f6fe9b1e89963d96842be92cce360b7976
Dan Plesa <dan.plesa@ifolor.com>
2020-09-24 15:47:18 +0200

    E2etest

--- RestfulBookerAutomatedTests/src/main/java/Main.java
+++ RestfulBookerAutomatedTests/src/main/java/Main.java
@@ -2,6 +2,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args){
+
         FirefoxDriver driver=new FirefoxDriver();
+        driver.get("http://localhost:8080/"):
+
+
     }
 }
